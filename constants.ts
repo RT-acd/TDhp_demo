b71962686ab0cd20be9e89324ba8b9dfee0ea144
RT-acd <72175463+RT-acd@users.noreply.github.com>
2026-01-15 11:20:48 +0900

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -62,11 +62,12 @@ export const SERVICES: ServiceItem[] = [
 export const COMPANY_INFO: CompanyInfo[] = [
   { label: '会社名', value: 'トップデンタル株式会社' },
   { label: '設立', value: '1995年4月1日' },
-  { label: '所在地', value: '〒371-0231 群馬県前橋市堀越町823-3' },
-  { label: '事業内容', value: '歯科医療機器の販売・修理・メンテナンス、歯科材料の販売、歯科医院開業・承継・閉業支援' },
-  { label: '加盟団体', value: 'XX会' },
-  { label: '資格', value: 'XX' },
-];
+  { label: '所在地', value: '〒371-0231 群馬県前橋市堀越町823' },
+  { label: '事業内容', value: '歯科医療機器・歯科材料の販売、歯科医院開業・承継・閉業支援' },
+  { label: '許可証', value: `高度管理医療機器等販売業・貸与業許可証　第F116号
+    医薬品販売業許可証　第E043号` },
+]
+  
 
 
 // CaseStudy型定義は残したまま、中身を空にします
