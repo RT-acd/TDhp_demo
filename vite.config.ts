2254cb40afc1a1cb74be16e2ecc5dc3ffb77cae8
RT-acd <72175463+RT-acd@users.noreply.github.com>
2026-06-13 14:44:02 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-    　base: '/TDhp_demo/',
+    　base: '/', // ★ '/TDhp_demo/' から '/' に変更します
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
